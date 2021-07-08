97e6d56ca5c49d1e394a5a0dc19815f3023195a3
root <nguyennamhainndc@gmail.com>
2021-07-08 17:59:00 +0000

    Fix 10^6

--- UIT20520171/Cplusplus/NKONEARC.cpp
+++ UIT20520171/Cplusplus/NKONEARC.cpp
@@ -1,63 +1,76 @@
 #include <cmath>
 #include <iostream>
+#include <vector>
 
 using namespace std;
-
 int main()
 {
-	int soMay, soKenh, mayBiTanCong, ketNoi[10000][2];
-    int danhSach[10000], duplicate, Count = 1, temp;
-    
+
+	int **ketNoi = new int*[10000000];
+	for (int i = 0; i< 10000000; i++){
+		ketNoi[i] = new int[2];
+	} 
+	int *danhSach = new int[10000000];
+	int soMay, soKenh, mayBiTanCong;
+	// Nhập 
 	cin >> soMay >> soKenh;
+
 	for (int i = 0; i < soKenh; i++) {
+		int x, y;
 		cin >> ketNoi[i][1];
 		cin >> ketNoi[i][0];
 	}
+	
 	cin >> mayBiTanCong;
 
-
+	// Chạy chương trình
+	int duplicate;
+	int count = 1;
 	danhSach[0] = mayBiTanCong;
 	for (int i = 0; i < soKenh; i++)
 	{
-		for (int i2 = 0; i2 < Count; i2++)
-		{
-			temp = danhSach[i2];
+		for (int i2 = 0; i2 < count; i2++)
+		{ 
+			int temp = danhSach[i2];
+			
 			if (temp == ketNoi[i][0])
 			{
 				duplicate = 0;
-				for (int i3 = 0; i3 < Count; i3++)
+				for (int i3 = 0; i3 < count; i3++)
 					if (danhSach[i3] == ketNoi[i][1])
 						duplicate = 1;
 				if (duplicate == 0)
 				{
-					danhSach[Count++] = ketNoi[i][1];
+					danhSach[count++] = ketNoi[i][1];
 				}
 			}
 			if (temp == ketNoi[i][1])
 			{
 				duplicate = 0;
-				for (int i3 = 0; i3 < Count; i3++)
+				for (int i3 = 0; i3 < count; i3++)
 					if (danhSach[i3] == ketNoi[i][0])
 						duplicate = 1;
 				if (duplicate == 0)
 				{
-					danhSach[Count++] = ketNoi[i][0];
+					danhSach[count++] = ketNoi[i][0];
 				}
 			}
 		}
 	}
 
-	for (int i = 0; i < Count - 1; i++)
+	for (int i = 0; i < count - 1; i++)
 	{
 		int min = i;
-		for (int j = i + 1; j < Count; j++)
+		for (int j = i + 1; j < count; j++)
 			if (danhSach[min] > danhSach[j])
 				min = j;
 		swap(danhSach[i], danhSach[min]);
 	}
 
-	cout<< Count <<endl;
-	for (int i = 0; i < Count; i++)
-		cout << danhSach[i]<<" ";
-    return 0;
+	// Xuất
+	cout<< count <<endl;
+	for (int i = 0; i < count; i++)
+		cout << danhSach[i] << " ";
+	
+	return 0;
 }
